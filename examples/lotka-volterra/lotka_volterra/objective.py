6993cae7084e770b548611b01859c2b36757c412
Mirko Hahn <mirhahn@ovgu.de>
2023-08-18 16:39:51 +0200

    Attached license boilerplate to all Python files.

--- examples/lotka-volterra/lotka_volterra/objective.py
+++ examples/lotka-volterra/lotka_volterra/objective.py
@@ -1,4 +1,5 @@
-# PyCoimset - Python library for set-valued optimization
+# PyCoimset Example "Lotka-Volterra": Problem-specific code
+#
 # Copyright 2023 Mirko Hahn
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
